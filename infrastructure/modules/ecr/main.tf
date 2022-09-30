5ec7cbebdf25aca6f4c953a787291a67d195b202
Gianmarco Ciaffardini <marco@Gianmarcos-MBP.homenet.telecomitalia.it>
2022-10-01 00:38:35 +0200

    chore: Add ecr lifecycle policy

--- infrastructure/modules/ecr/main.tf
+++ infrastructure/modules/ecr/main.tf
@@ -30,20 +30,20 @@ resource "aws_ecr_lifecycle_policy" "expire_policy" {
 
   policy = <<EOF
   {
-      "rules": [
-          {
-              "rulePriority": 1,
-              "description": "Keep last 5 images",
-              "selection": {
-                  "tagStatus": "any",
-                  "countType": "imageCountMoreThan",
-                  "countNumber": 5
-              },
-              "action": {
-                  "type": "expire"
-              }
-          }
-      ]
+    "rules": [
+      {
+        "rulePriority": 1,
+        "description": "keep 3 images",
+        "selection": {
+          "tagStatus": "any",
+          "countType": "imageCountMoreThan",
+          "countNumber": 3
+        },
+        "action": {
+          "type": "expire"
+        }
+      }
+    ]
   }
   EOF
 }
