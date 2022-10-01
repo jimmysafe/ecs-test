b79009b3e154c5825848bbbffd32a4186aea03d7
Gianmarco Ciaffardini <marco@Gianmarcos-MBP.homenet.telecomitalia.it>
2022-10-01 17:08:43 +0200

    chore: final try

--- infrastructure/modules/vpc/ main.tf
+++ infrastructure/modules/vpc/ main.tf
@@ -93,3 +93,28 @@ resource "aws_route_table_association" "public_subnet_2-route-table-association"
   route_table_id = aws_route_table.public_route_table.id
 }
 
+
+#  PRIVATE
+
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.main.id  
+  tags = {
+    "Name" = "${local.name}-pvt-route-table"
+  }
+}
+
+resource "aws_route" "private_route" {
+  route_table_id              = "${aws_route_table.private_route_table.id}"
+  destination_cidr_block                  = "0.0.0.0/0"
+  gateway_id                  = aws_internet_gateway.internet_gateway.id
+}
+
+resource "aws_route_table_association" "private_subnet_1-route-table-association" {
+  subnet_id = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_subnet_2-route-table-association" {
+  subnet_id = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
